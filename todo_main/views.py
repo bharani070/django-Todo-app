32eaeba3ca2e84f16f523d34f8c0a907b3167998
bharani <bharanikumar070@gmail.com>
2023-06-20 17:14:54 +0530

    mark as done or not done

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,9 @@ from todo.models import Task
 
 def home(request):
     task = Task.objects.filter(is_completed=False).order_by('-updated_At')
+    completed_Tasks = Task.objects.filter(is_completed=True).order_by('-updated_At')
     context = {
         'tasks' : task,
+        'completed_Tasks' : completed_Tasks
     }
     return render(request, 'home.html', context)
